56650295847267c9cf9ad0721f2ec836efaf2a63
Sergei Demchenko <hb13718@gmail.com>
2024-11-21 10:41:55 +0300

    Delete commented code

--- task1/solution.py
+++ task1/solution.py
@@ -1,35 +1,12 @@
 from functools import wraps
 
 
-# def strict(func):
-#     @wraps(func)
-#     def wrapper(*args, **kwargs):
-#         annotations = func.__annotations__
-#         param_names = list(annotations.keys())[:-1]
-#
-#         for arg_name, arg_value in zip(param_names, args):
-#             expected_type = annotations[arg_name]
-#             if not isinstance(arg_value, expected_type):
-#                 raise TypeError(
-#                     f"Argument '{arg_name}' must be of type {expected_type.__name__}, "
-#                     f"but got {type(arg_value).__name__}."
-#                 )
-#
-#         return func(*args, **kwargs)
-#
-#     return wrapper
-
-
-from functools import wraps
-
-
 def strict(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         annotations = func.__annotations__
-        param_names = list(annotations.keys())[:-1]  # Исключаем возвращаемый тип (последний аннотационный параметр)
+        param_names = list(annotations.keys())[:-1]
 
-        # Проверяем позиционные аргументы
         for arg_name, arg_value in zip(param_names, args):
             if arg_name not in annotations:
                 continue
@@ -40,7 +17,6 @@ def strict(func):
                     f"but got {type(arg_value).__name__}."
                 )
 
-        # Проверяем именованные аргументы
         for kwarg_name, kwarg_value in kwargs.items():
             if kwarg_name not in annotations:
                 continue
@@ -54,21 +30,3 @@ def strict(func):
         return func(*args, **kwargs)
 
     return wrapper
-
-# def strict2(func):
-#     annotations = func.__annotations__
-#     param_names = list(annotations.keys())[:-1]
-#
-#     def wrapper(*args, **kw):
-#
-#
-#         return func(*args, **kw)
-#
-#     return wrapper
-#
-# @strict2
-# def foo(a: int, b: int) -> int:
-#     return a + b
-#
-#
-# a = foo(2, 12)
